76f4fd2d1d0e38d4d818656a98e68980d67e36d2
Malvina <malvinahohlova@yandex.ru>
2026-06-18 18:35:23 +0300

    Домашка 5

--- lesson_5/test_lesson05_task2.py
+++ lesson_5/test_lesson05_task2.py
@@ -2,18 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+
 def test_form_submission():
     driver = webdriver.Chrome()
     driver.get("https://httpbin.org/forms/post")
-    time.sleep(2) 
+    time.sleep(2)
 
     name_input = driver.find_element(By.NAME, "custname")
-    name_input.send_keys("Мальвина") 
+    name_input.send_keys("Мальвина")
 
-    submit_button = driver.find_element(By.XPATH, "//button[text()='Submit order']")
+    xpath = "//button[text()='Submit order']"
+    submit_button = driver.find_element(By.XPATH, xpath)
     submit_button.click()
 
-    time.sleep(3) 
+    time.sleep(3)
 
     if driver.current_url != "https://httpbin.org/forms/post":
         print("URL успешно изменился.")
@@ -21,6 +23,7 @@ def test_form_submission():
         print("URL остался прежним.")
 
     driver.quit()
+
 
 if __name__ == "__main__":
     test_form_submission()
