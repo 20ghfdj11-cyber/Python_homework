ca709877ef1f1801dbe88e50f8fdbdc41700ece0
Malvina <malvinahohlova@yandex.ru>
2026-07-10 20:20:15 +0300

    Домашка №7

---
+++ lesson_7/test_calculator.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from calculator_page import CalculatorPage
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+def test_calculator(driver):
+    url = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+    calc_page = CalculatorPage(driver, url)
+    calc_page.open()
+    calc_page.set_delay()
+    calc_page.enter_expression()
+    calc_page.get_result()
+    assert calc_page.get_result() == "15"
